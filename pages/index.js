36d1a8f90f6b1818b9fa37157fae6270e2f4d360
Gabriel Galvão de Oliveira <104661811+anxexcalibur@users.noreply.github.com>
2025-07-20 21:42:01 +0000

    ..

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,15 @@
 function Home() {
   return (
     <div>
-      <h1>O egoísmo é uma prisão silenciosa 🧱🤐,
-a inveja, um fogo que consome o próprio coração 🔥💔.
-O traidor não saberá receber amor ❌❤️,
-pois o preço da traição é a perda da confiança,
-a solidão da alma e o vazio que nem o tempo pode preencher. ⏳💔
-Tudo tem um preço — às vezes, é a própria alegria da alma 🎈✨.
-Seja íntegro — respeite as almas que cruzam seu caminho 🤝🌟.
-</h1>
+      <h1>
+        O egoísmo é uma prisão silenciosa 🧱🤐,<br />
+        a inveja, um fogo que consome o próprio coração 🔥💔.<br />
+        O traidor não saberá receber amor ❌❤️,<br />
+        pois o preço da traição é a perda da confiança,<br />
+        a solidão da alma e o vazio que nem o tempo pode preencher. ⏳💔<br />
+        Tudo tem um preço — às vezes, é a própria alegria da alma 🎈✨.<br />
+        Seja íntegro — respeite as almas que cruzam seu caminho 🤝🌟.
+      </h1>
     </div>
   );
 }
