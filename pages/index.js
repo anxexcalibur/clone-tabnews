223a5a8fe71428be8335118098ba5afc0c3448b0
Gabriel Galvão de Oliveira <104661811+anxexcalibur@users.noreply.github.com>
2025-07-20 21:25:54 +0000

    salada

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,9 @@
-function Home(){
-    return 
+function Home() {
+  return (
     <div>
-        
-        <h1>Não te aflias pelo que não podes controlar; 🌿 a vida segue seu curso e tudo se revela no tempo certo. ⏳✨ Tenha fé em sua jornada. 🙏💫</h1>
+      <h1>Não te aflijas pelo que não podes controlar; 🌿 a vida segue seu curso e tudo se revela no tempo certo. ⏳✨ Tenha fé em sua jornada. 🙏💫</h1>
     </div>
+  );
 }
-    
 
 export default Home;
