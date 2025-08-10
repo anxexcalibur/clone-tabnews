99fb376fa6075e4cfeb0d407d89d78a7ece8109d
Gabriel Galvão de Oliveira <104661811+anxexcalibur@users.noreply.github.com>
2025-08-10 19:00:24 +0000

    público alvo

---
+++ pages/index.jsx
@@ -0,0 +1,58 @@
+function Home() {
+  return (
+    <div>
+      <h1>Perfil do Público do Esotab.com</h1>
+      <p>
+        O <b>Esotab.com</b> é para pessoas que buscam o equilíbrio entre razão e{" "}
+        <br />
+        sensibilidade — indivíduos exatos e empáticos que valorizam o<br />
+        conhecimento fundamentado e a intuição.
+        <br />
+      </p>
+      <h2>Nosso público ideal:</h2>
+      <ul>
+        <li>
+          <p>
+            Tem um compromisso sincero com o crescimento espiritual e o
+            despertar da consciência.
+          </p>
+        </li>
+        <li>
+          <p>
+            Busca um espaço seguro para compartilhar e aprender sem medo de
+            julgamentos ou dogmatismos.
+          </p>
+        </li>
+        <li>
+          <p>
+            Valoriza a integridade pessoal e a honestidade intelectual, prezando
+            pela clareza e profundidade nas discussões.
+          </p>
+        </li>
+        <li>
+          <p>
+            Está aberto ao diálogo respeitoso sobre tradições, filosofias e
+            sabedorias antigas, reconhecendo a riqueza das diversas expressões
+            espirituais.
+          </p>
+        </li>
+        <li>
+          <p>
+            Deseja contribuir para uma comunidade pautada pela justiça, pela
+            ética e pelo apoio mútuo.
+          </p>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+function teste() {
+  console.log("teste");
+}
+
+function teste2() {
+  console.log("sdad");
+}
+
+export default Home;
